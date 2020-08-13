eb6c51f80f12d1ed3a4e83822d0daed4c55ed01d
tripathi21 <41079931+tripathi21@users.noreply.github.com>
2020-08-13 21:47:31 +0530

    Update sell-history.component.ts

--- src/app/sell-history/sell-history.component.ts
+++ src/app/sell-history/sell-history.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sell-history',
-  templateUrl: './sell-history.component.html',
-  styleUrls: ['./sell-history.component.css']
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
 })
-export class SellHistoryComponent implements OnInit {
+export class AppComponent  {
 
-  constructor() { }
+    columnDefs = [
+        {field: 'Date' },
+        {field: 'Crop_Name' },
+        {field: 'Quantity'},
+        {field: 'MSP'},
+        {field: 'Sold_Price'},
+        {field: 'Total_Price'}
+    ];
 
-  ngOnInit(): void {
-  }
+    rowData = [
+        { Date: '11-08-2020', Crop_Name: 'Rice', Quantity: 15, MSP: 1500, Sold_Price: 2000, Total_Price: 20000 },
+        { Date: '11-08-2020', Crop_Name: 'Rice', Quantity: 15, MSP: 1500, Sold_Price: 2000, Total_Price: 20000 },
+        { Date: '11-08-2020', Crop_Name: 'Rice', Quantity: 15, MSP: 1500, Sold_Price: 2000, Total_Price: 20000 }
+        
+    ];
 
 }
