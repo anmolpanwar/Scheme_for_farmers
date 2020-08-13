0e34ed29368085cb0852814d07c6be62e6fd8b9f
tripathi21 <41079931+tripathi21@users.noreply.github.com>
2020-08-13 22:54:07 +0530

    Update sell-history.component.ts

--- src/app/sell-history/sell-history.component.ts
+++ src/app/sell-history/sell-history.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  selector: 'app-sell-history',
+  templateUrl: './sell-history.component.html',
+  styleUrls: ['./sell-history.component.css']
 })
-export class AppComponent  {
+export class  SellHistoryComponent {
 
     columnDefs = [
         {field: 'Date' },
